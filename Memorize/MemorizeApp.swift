9158129ec4bb3bd102577290650e55c353c6a42b
restfulyams <calvin.mwariama@embue.com>
2024-01-30 22:48:16 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Calvin Mwariama on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
